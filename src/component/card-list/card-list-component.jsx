ba3d4977727788b0bb00a6fd542a9c47cdabbe4d
Monzer Mohammad <dev.monzer@gmail.com>
2022-01-31 13:07:47 +0300

    Update card,card-list components

--- src/component/card-list/card-list-component.jsx
+++ src/component/card-list/card-list-component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import './card-list-styles.css'
+import { Card } from '../card/card.component';
 
-export const CardList = props => {
-  return <div className='card-list'>
-       {props.monsters.map(monster => 
-          <h1 key={monster.id}>{monster.name}</h1>
-        )}      
-</div>
-};
+import './card-list.styles.css';
+
+export const CardList = props => (
+  <div className='card-list'>
+    {props.monsters.map(monster => (
+      <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+);
