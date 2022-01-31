73997211f0db9f1139ef33ee60b282a0b8ca6edd
Monzer Mohammad <dev.monzer@gmail.com>
2022-01-31 12:14:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,12 +18,11 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <CardList/>
-        <header className="App-header">
+        <CardList>
           {this.state.monsters.map(monster => 
               <h1 key={monster.id}>{monster.name}</h1>
             )}
-        </header>
+        </CardList>          
       </div>
     );
   }
