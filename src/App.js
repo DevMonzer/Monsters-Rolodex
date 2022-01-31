7164a074b8e5d58ee294d7327334b33168123296
Monzer Mohammad <dev.monzer@gmail.com>
2022-01-31 11:50:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,17 +15,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-        </a>
+
              <p>{this.state.string}</p>
             <button onClick={() => this.setState({string: 'hello Monzer'})}>Change Text</button>
             <button onClick={() => this.setState({string: ' Monzer'})}>Change Text</button>
