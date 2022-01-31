b5d7e819bca951345267b730deb29b1251e72e32
Monzer Mohammad <dev.monzer@gmail.com>
2022-01-31 12:59:33 +0300

    Update App and card-list

--- src/App.js
+++ src/App.js
@@ -18,10 +18,7 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <CardList>
-          {this.state.monsters.map(monster => 
-              <h1 key={monster.id}>{monster.name}</h1>
-            )}
+        <CardList monsters={this.state.monsters}>
         </CardList>          
       </div>
     );
--- src/component/card-list/card-list-component.jsx
+++ src/component/card-list/card-list-component.jsx
@@ -3,5 +3,9 @@ import React from 'react';
 import './card-list-styles.css'
 
 export const CardList = props => {
-  return <div className='card-list'>{props.childern}</div>
+  return <div className='card-list'>
+       {props.monsters.map(monster => 
+          <h1 key={monster.id}>{monster.name}</h1>
+        )}      
+</div>
 };
